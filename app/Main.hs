8bce9791b11919fa3943a439a5d5969f984f9eae
Denis Tereshkin <denis@kasan.ws>
2016-09-22 00:42:23 +0700

    Config support

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,7 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Lib
-import QuoteSource.DDE
 import QuoteSource.DataImport
 import Control.Concurrent hiding (readChan)
 import Control.Monad
@@ -13,11 +13,68 @@ import Data.ATrade
 import QuoteSource.TableParsers.AllParamsTableParser
 import QuoteSource.TableParser
 
-callback :: DdeCallback
-callback = undefined
+import System.Log.Logger
+import System.Log.Handler.Simple
+import System.Log.Handler (setFormatter)
+import System.Log.Formatter
+
+import Data.Aeson
+import Data.Aeson.Types
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.HashMap.Strict as HM
+import qualified Data.Vector as V
+import qualified Data.Text as T
+
+data TableConfig = TableConfig {
+  parserId :: String,
+  tableName :: String,
+  tableParams :: Value
+} deriving (Show)
+
+data Config = Config {
+  quotesourceEndpoint :: String,
+  brokerserverEndpoint :: String,
+  tables :: [TableConfig]
+} deriving (Show)
+
+readConfig :: String -> IO Config
+readConfig fname = do
+  content <- BL.readFile fname
+  case decode content >>= parseMaybe parseConfig of
+    Just config -> return config
+    Nothing -> error "Unable to load config"
+
+parseConfig :: Value -> Parser Config
+parseConfig = withObject "object" $ \obj -> do
+  qse <- obj .: "quotesource-endpoint"
+  bse <- obj .: "brokerserver-endpoint"
+  rt <- case HM.lookup "tables" obj of
+    Just v -> parseTables v
+    Nothing -> fail "Expected tables array"
+  return Config { quotesourceEndpoint = qse,
+    brokerserverEndpoint = bse,
+    tables = rt }
+  where
+    parseTables :: Value -> Parser [TableConfig]
+    parseTables = withArray "array" $ \arr -> mapM parseTableConfig (V.toList arr)
+
+    parseTableConfig :: Value -> Parser TableConfig
+    parseTableConfig = withObject "object" $ \obj -> do
+      pid <- obj .: "parser-id"
+      tn <- obj .: "table-name"
+      params <- case HM.lookup "params" obj of
+        Just x -> return x
+        Nothing -> return $ Object HM.empty
+      return TableConfig {
+        parserId = pid,
+        tableName = tn,
+        tableParams = params }
+
 
 main :: IO ()
 main = do
+  config <- readConfig "quik-connector.config.json"  
+  print config
   chan <- newBoundedChan 1000
   forkIO $ forever $ do
     tick <- readChan chan
